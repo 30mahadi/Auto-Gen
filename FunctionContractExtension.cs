4e985c3655a94b8b2ae4287e05340f93c8995570
Mahadi Mokbul Ali <125756057+30mahadi@users.noreply.github.com>
2026-08-08 22:52:10 +0600

    Add files via upload

--- FunctionContractExtension.cs
+++ FunctionContractExtension.cs
@@ -1,90 +1,54 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // FunctionContractExtension.cs
 
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using AutoGen.Core;
-using Google.Cloud.AIPlatform.V1;
 using Json.Schema;
 using Json.Schema.Generation;
-using OpenAPISchemaType = Google.Cloud.AIPlatform.V1.Type;
-using Type = System.Type;
 
-namespace AutoGen.Gemini.Extension;
+namespace AutoGen.Mistral.Extension;
 
 public static class FunctionContractExtension
 {
     /// <summary>
-    /// Convert a <see cref="FunctionContract"/> to a <see cref="FunctionDeclaration"/> that can be used in gpt funciton call.
+    /// Convert a <see cref="FunctionContract"/> to a <see cref="FunctionDefinition"/> that can be used in funciton call.
     /// </summary>
-    public static FunctionDeclaration ToFunctionDeclaration(this FunctionContract function)
+    /// <param name="functionContract">function contract</param>
+    /// <returns><see cref="FunctionDefinition"/></returns>
+    public static FunctionDefinition ToMistralFunctionDefinition(this FunctionContract functionContract)
     {
-        var required = function.Parameters!.Where(p => p.IsRequired)
-                    .Select(p => p.Name)
-                    .ToList();
-        var parameterProperties = new Dictionary<string, OpenApiSchema>();
-
-        foreach (var parameter in function.Parameters ?? Enumerable.Empty<FunctionParameterContract>())
+        var functionDefinition = new FunctionDefinition(functionContract.Name ?? throw new Exception("Function name cannot be null"), functionContract.Description ?? throw new Exception("Function description cannot be null"));
+        var requiredParameterNames = new List<string>();
+        var propertiesSchemas = new Dictionary<string, JsonSchema>();
+        var propertySchemaBuilder = new JsonSchemaBuilder().Type(SchemaValueType.Object);
+        foreach (var param in functionContract.Parameters ?? [])
         {
-            var schema = ToOpenApiSchema(parameter.ParameterType);
-            schema.Description = parameter.Description;
-            schema.Title = parameter.Name;
-            schema.Nullable = !parameter.IsRequired;
-            parameterProperties.Add(parameter.Name!, schema);
-        }
-
-        return new FunctionDeclaration
-        {
-            Name = function.Name,
-            Description = function.Description,
-            Parameters = new OpenApiSchema
+            if (param.Name is null)
             {
-                Required =
-                        {
-                            required,
-                        },
-                Properties =
-                        {
-                            parameterProperties,
-                        },
-                Type = OpenAPISchemaType.Object,
-            },
-        };
-    }
-
-    private static OpenApiSchema ToOpenApiSchema(Type? type)
-    {
-        if (type == null)
-        {
-            return new OpenApiSchema
-            {
-                Type = OpenAPISchemaType.Unspecified
-            };
-        }
-
-        var schema = new JsonSchemaBuilder().FromType(type).Build();
-        var openApiSchema = new OpenApiSchema
-        {
-            Type = schema.GetJsonType() switch
-            {
-                SchemaValueType.Array => OpenAPISchemaType.Array,
-                SchemaValueType.Boolean => OpenAPISchemaType.Boolean,
-                SchemaValueType.Integer => OpenAPISchemaType.Integer,
-                SchemaValueType.Number => OpenAPISchemaType.Number,
-                SchemaValueType.Object => OpenAPISchemaType.Object,
-                SchemaValueType.String => OpenAPISchemaType.String,
-                _ => OpenAPISchemaType.Unspecified
-            },
-        };
-
-        if (schema.GetJsonType() == SchemaValueType.Object && schema.GetProperties() is var properties && properties != null)
-        {
-            foreach (var property in properties)
-            {
-                openApiSchema.Properties.Add(property.Key, ToOpenApiSchema(property.Value.GetType()));
+                throw new InvalidOperationException("Parameter name cannot be null");
             }
-        }
 
-        return openApiSchema;
+            var schemaBuilder = new JsonSchemaBuilder().FromType(param.ParameterType ?? throw new ArgumentNullException(nameof(param.ParameterType)));
+            if (param.Description != null)
+            {
+                schemaBuilder = schemaBuilder.Description(param.Description);
+            }
+
+            if (param.IsRequired)
+            {
+                requiredParameterNames.Add(param.Name);
+            }
+
+            var schema = schemaBuilder.Build();
+            propertiesSchemas[param.Name] = schema;
+
+        }
+        propertySchemaBuilder = propertySchemaBuilder.Properties(propertiesSchemas);
+        propertySchemaBuilder = propertySchemaBuilder.Required(requiredParameterNames);
+
+        functionDefinition.Parameters = propertySchemaBuilder.Build();
+
+        return functionDefinition;
     }
 }
