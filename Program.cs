502d8b5d17b16d819f64f8595e06f72eeb3b59a5
Mahadi Mokbul Ali <125756057+30mahadi@users.noreply.github.com>
2026-08-08 22:58:41 +0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Program.cs
-using Microsoft.Extensions.Hosting;
 
-var app = await Microsoft.AutoGen.RuntimeGateway.Grpc.Host.StartAsync(local: false, useGrpc: true).ConfigureAwait(false);
-await app.WaitForShutdownAsync();
+Console.WriteLine("Hello, World!");
